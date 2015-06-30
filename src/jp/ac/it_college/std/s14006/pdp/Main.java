2423f1d4c9bf48a79d4e5e8949c31947966e6834
s14006 <s14006@std.it-college.ac.jp>
2015-06-30 12:58:25 +0900

    Initial commit

---
+++ src/jp/ac/it_college/std/s14006/pdp/Main.java
@@ -0,0 +1,51 @@
+package jp.ac.it_college.std.s14006.pdp;
+
+import jp.ac.it_college.std.nakasone.lib.exam.Communicator;
+import java.io.IOException;
+import java.util.Scanner;
+
+
+/**
+ * Created by kabotya on 15/06/28.
+ */
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Communicator comm = new Communicator((msg) -> {
+            System.out.print(msg);
+        });
+        comm.start();
+
+        try {
+
+            System.out.println("名前を入力");
+            Scanner scanner = new Scanner(System.in);
+            String name = scanner.next();
+            comm.send(name + "が入室しました");
+
+            while (true) {
+                Scanner scan = new Scanner(System.in);
+                String smsg = scan.nextLine();
+
+                if (smsg.equals("exit")) {
+                    comm.send(name + "が退出しました");
+                    comm.finish();
+                    break;
+                } else if (smsg.equals("change")) {
+                    Scanner change = new Scanner(System.in);
+                    comm.send(name + "が" + change + "になりました");
+                    name = change.next();
+                } else {
+                    comm.send(name + ":" + smsg);
+                }
+
+            }
+
+        } catch (IOException var3) {
+            var3.printStackTrace();
+        }
+
+    }
+}
